856e19549d45619ca2d3b0b2d38dfa7f0923e5f7
manirajpt <mani90033721@gmail.com>
2024-06-19 17:54:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const serverless = require('serverless-http');
 const connectDB=require("./config/dbconfig")
 
 dotenv.config();
